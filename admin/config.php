219fe3027c9ada06758da935ef1192ef7de7b3ba
qcollinscode <qcollins@qraynix.com>
2019-08-09 14:56:17 -0500

    first commit

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ecommerce_site/admin/');
+define('HTTP_CATALOG', 'http://localhost/ecommerce_site/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ecommerce_site/admin/');
+define('HTTPS_CATALOG', 'http://localhost/ecommerce_site/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/admin/Sites/ecommerce_site/admin/');
+define('DIR_SYSTEM', '/Users/admin/Sites/ecommerce_site/system/');
+define('DIR_IMAGE', '/Users/admin/Sites/ecommerce_site/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', '/Users/admin/Sites/ecommerce_site/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '50cent');
+define('DB_DATABASE', 'ecommerce_site');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ecommerce_site/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ecommerce_site/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/admin/Sites/ecommerce_site/catalog/');
+define('DIR_SYSTEM', '/Users/admin/Sites/ecommerce_site/system/');
+define('DIR_IMAGE', '/Users/admin/Sites/ecommerce_site/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '50cent');
+define('DB_DATABASE', 'ecommerce_site');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
